c3fc0caee5245e7fccbe15ee0aa35b25994c1949
Andrew <andrew.lewis@interapthq.com>
2016-11-16 10:58:35 -0500

    Status Classes made

---
+++ app/src/main/java/PeopleGatchi/PeopleGatchi/Status/Bladder.java
@@ -0,0 +1,17 @@
+package PeopleGatchi.PeopleGatchi.Status;
+
+/**
+ * Created by andrewlewis on 11/16/16.
+ */
+
+public class Bladder {
+    //bladder level down, happiness goes up.
+
+    public void bladderLevelUp () {
+
+        bladderLevel += 2;
+        happinessLevel -= 2;
+    }
+    //bladder level up, happiness goes down.
+
+}
---
+++ app/src/main/java/PeopleGatchi/PeopleGatchi/Status/Hunger.java
@@ -0,0 +1,8 @@
+package PeopleGatchi.PeopleGatchi.Status;
+
+/**
+ * Created by andrewlewis on 11/16/16.
+ */
+
+public class Hunger {
+}
