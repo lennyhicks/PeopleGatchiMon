f467436ed479a2a2503e45cd3f2129d22334a7dc
CrystalJewell <Crystal.Adkins@interapthq.com>
2016-11-17 16:39:15 -0500

    Working on Inventory View, mostly working need to populate array to verify

--- app/src/main/java/com/PeopleGatchi/Adapters/InventoryAdapter.java
+++ app/src/main/java/com/PeopleGatchi/Adapters/InventoryAdapter.java
@@ -5,13 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.PeopleGatchi.R;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 
 
 
@@ -21,58 +17,52 @@ import java.util.ArrayList;
 
 public class InventoryAdapter extends BaseAdapter {
 
-    private ArrayList<Object> items;
-    private LayoutInflater layoutInflater;
-//    private SimpleDateFormat formatter;
+        private Context context;
+        private final int[] images;
 
-    // Construct our custom adapter
-    public InventoryAdapter(Context context, ArrayList<Object> object) {
-        this.items = object;
-        this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        formatter = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
-    }
-
-    // Get the number of views we will need to inflate. Should be the size of our items array
-    @Override
-    public int getCount() {
-        return items.size();
-    }
-
-    // Get the position in our array we are at
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    // Get the position of our item in the array
-    @Override
-    public Object getItem(int position) {
-        return items.get(position);
-    }
-
-    // Determine the amount of separate views our adapter will need to handle
-    @Override
-    public int getViewTypeCount() {
-        return 2;
-    }
-
-    /*
-    Determine the type of view being created and insert data
-    */
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        if (convertView == null) {
-
-                convertView = layoutInflater.inflate(R.layout.inventory_item, parent, false);
+        public InventoryAdapter(Context context, int[] images) {
+            this.context = context;
+            this.images = images;
         }
 
-        GridView inventoryItem = (GridView) convertView.findViewById(R.id.inventory_grid);
+        public View getView(int position, View convertView, ViewGroup parent) {
 
-            inventoryItem.//Need to figure out how to make things show up on the 
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+            View gridView;
 
+            if (convertView == null) {
 
-        return convertView;
+                gridView = new View(context);
+
+                gridView = inflater.inflate(R.layout.inventory_view, null);
+
+                ImageView imageView = (ImageView) gridView.findViewById(R.id.inventory_img);
+
+                int itemImages = images[position];
+
+                imageView.setImageResource(itemImages);
+
+            } else {
+                gridView = (View) convertView;
+            }
+
+            return gridView;
+        }
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
     }
-}
--- app/src/main/java/com/PeopleGatchi/Dialogs/InventoryDialog.java
+++ app/src/main/java/com/PeopleGatchi/Dialogs/InventoryDialog.java
@@ -1,9 +1,48 @@
 package com.PeopleGatchi.Dialogs;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.PeopleGatchi.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by crystaladkins on 11/16/16.
  */
 
-public class InventoryDialog {
+public class InventoryDialog extends Dialog{
+
+
+
+
+    public InventoryDialog(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.inventory_view);
+
+        ButterKnife.bind(this);
+
+
+        }
+
+    @OnClick(R.id.inventory_img)
+    public void itemSelected () {
+
+    }
+
+
+//    ImageAdapter adapter = new ImageAdapter(this, home_res);
+//
+//    gameGrid.setAdapter(adapter);
 
 }
