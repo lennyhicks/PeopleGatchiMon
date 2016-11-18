963f65550445eb689cb74481c902e2a25bb4cc61
SheaMaynard <weirdalgurl@yahoo.com>
2016-11-18 13:24:55 -0500

    Merge branch 'development' of https://github.com/lennyhicks/PeopleGatchiMon into feature/Shea

--- app/src/main/java/com/PeopleGatchi/Dialogs/InventoryDialog.java
+++ app/src/main/java/com/PeopleGatchi/Dialogs/InventoryDialog.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
 
@@ -67,7 +68,11 @@ public class InventoryDialog extends Dialog{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.inventory_view);
 
         ButterKnife.bind(this);
--- app/src/main/java/com/PeopleGatchi/MainActivity.java
+++ app/src/main/java/com/PeopleGatchi/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
 import android.widget.RelativeLayout;
 
 import com.PeopleGatchi.Network.UserStore;
@@ -35,7 +36,11 @@ public class MainActivity extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+
+
 
         setContentView(activity_main);
 
