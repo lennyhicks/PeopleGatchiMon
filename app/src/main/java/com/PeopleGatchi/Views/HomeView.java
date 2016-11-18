d8f67c13b6f3a6d9412dd4fb33dcc60a9d5152ad
SheaMaynard <weirdalgurl@yahoo.com>
2016-11-18 08:40:29 -0500

    Merge branch 'development' of https://github.com/lennyhicks/PeopleGatchiMon into feature/Shea
    # Conflicts:
    #	app/src/main/java/com/PeopleGatchi/Views/HomeView.java

--- app/src/main/java/com/PeopleGatchi/Views/HomeView.java
+++ app/src/main/java/com/PeopleGatchi/Views/HomeView.java
@@ -7,7 +7,6 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.PeopleGatchi.PeopleGatchiApplication;
 import com.PeopleGatchi.R;
@@ -16,6 +15,7 @@ import com.PeopleGatchi.Stages.InventoryStage;
 import com.PeopleGatchi.Stages.JobStage;
 import com.PeopleGatchi.Stages.StoreStage;
 import com.PeopleGatchi.Utils.StatusControls;
+import com.PeopleGatchi.Utils.Utils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -89,13 +89,21 @@ public class HomeView extends RelativeLayout {
 
         StatusControls.firstRun();
         foodBar.setProgress(StatusControls.getHungerLevel());
+//        foodBar.setIndeterminate(false);
+//        foodBar.setSecondaryProgress(75);
+
         drinkBar.setProgress(StatusControls.getHygieneLevel());
+//        drinkBar.setIndeterminate(false);
+//        drinkBar.setSecondaryProgress(75);
+
         hygieneBar.setProgress(StatusControls.getThirstLevel());
         peeBar.setProgress(StatusControls.getPeeLevel());
         poopBar.setProgress(StatusControls.getPooLevel());
         sleepBar.setProgress(StatusControls.getRestLevel());
 
-        Toast.makeText(context, StatusControls.getPooLevel(), Toast.LENGTH_LONG).show();
+        imageView.setImageResource(Utils.setHappinessImage());
+
+//        Toast.makeText(context, StatusControls.getPooLevel(), Toast.LENGTH_LONG).show();
 
        // EventBus.getDefault().register(this);
     }
@@ -152,21 +160,18 @@ public class HomeView extends RelativeLayout {
 
     @OnClick(R.id.store_button)
     public void goToStore(){
-
         History newHistory = flow.getHistory().buildUpon().push(new StoreStage()).build();
         flow.setHistory(newHistory, Flow.Direction.FORWARD);
     }
 
     @OnClick(R.id.inventory_button)
     public void goToInventory(){
-
         History newHistory = flow.getHistory().buildUpon().push(new InventoryStage()).build();
         flow.setHistory(newHistory, Flow.Direction.FORWARD);
     }
 
     @OnClick(R.id.education_button)
     public void goToSchool(){
-        //History newHistory = History.single(new EducationStage());
         History newHistory = flow.getHistory().buildUpon().push(new EducationStage()).build();
         flow.setHistory(newHistory, Flow.Direction.FORWARD);
 
@@ -174,10 +179,10 @@ public class HomeView extends RelativeLayout {
 
     @OnClick(R.id.work_button)
     public void goToWork(){
-        //History newHistory = History.single(new JobStage());
         History newHistory = flow.getHistory().buildUpon().push(new JobStage()).build();
         flow.setHistory(newHistory, Flow.Direction.FORWARD);
 
     }
+
 
 }
