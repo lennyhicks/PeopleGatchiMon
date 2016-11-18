3d517875070fda927c318d6cd342b39cd14680be
SheaMaynard <weirdalgurl@yahoo.com>
2016-11-18 14:14:49 -0500

    Merge branch 'development' of https://github.com/lennyhicks/PeopleGatchiMon into feature/Shea
    # Conflicts:
    #	app/src/main/res/layout/inventory_view.xml
    #	app/src/main/res/layout/store_view.xml

--- app/src/main/java/com/PeopleGatchi/MainActivity.java
+++ app/src/main/java/com/PeopleGatchi/MainActivity.java
@@ -3,8 +3,6 @@ package com.PeopleGatchi;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.View;
-import android.view.Window;
 import android.widget.RelativeLayout;
 
 import com.PeopleGatchi.Network.UserStore;
@@ -34,12 +32,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 
 
@@ -80,12 +78,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //It's your boy the menu button! I made it a little star!
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        this.menu = menu;
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        this.menu = menu;
+//        return true;
+//    }
 
 
 }
