107a8b3f06c4ed68717bd8d3efe83482b7219a33
Lenny Hicks <lenny.hicks@interapthq.com>
2016-11-17 22:25:12 -0500

    Updated Status Controls

--- app/src/main/java/com/PeopleGatchi/Utils/StatusControls.java
+++ app/src/main/java/com/PeopleGatchi/Utils/StatusControls.java
@@ -160,6 +160,18 @@ public class StatusControls {
         hygiene.save();
         rest.save();
 
+        updateLevels();
         getLevels();
+    }
+
+    public static void updateLevels(){
+
+
+        pooBladder = Poo.findById(Poo.class, 1);
+        peeBladder = Pee.findById(Pee.class, 1);
+        hunger = Hunger.findById(Hunger.class, 1);
+        thirst = Thirst.findById(Thirst.class, 1);
+        hygiene = Hygiene.findById(Hygiene.class, 1);
+        rest = Rest.findById(Rest.class, 1);
     }
 }
