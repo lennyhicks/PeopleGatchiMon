3b24de6ae3fbc3464250dacf57d60a52a8bd4305
CrystalJewell <Crystal.Adkins@interapthq.com>
2016-11-18 15:42:42 -0500

    FIRST RUN WORKING!!!

--- app/src/main/java/com/PeopleGatchi/MainActivity.java
+++ app/src/main/java/com/PeopleGatchi/MainActivity.java
@@ -1,10 +1,11 @@
 package com.PeopleGatchi;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.RelativeLayout;
 
-import com.PeopleGatchi.Network.UserStore;
 import com.PeopleGatchi.Stages.CreateStage;
 import com.PeopleGatchi.Stages.HomeStage;
 import com.davidstemmer.flow.plugin.screenplay.ScreenplayDispatcher;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = "MainActivity";
     private Flow flow;
     private ScreenplayDispatcher dispatcher;
+    private SharedPreferences peoplegatchiPrefs;
 
     @Bind(R.id.container)
     RelativeLayout container;
@@ -30,15 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        getWindow().getDecorView().setSystemUiVisibility(
-//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-
-
+        peoplegatchiPrefs = getPreferences(Context.MODE_PRIVATE);
 
         setContentView(activity_main);
 
@@ -54,7 +48,11 @@ public class MainActivity extends AppCompatActivity {
         dispatcher = new ScreenplayDispatcher(this, container);
         dispatcher.setUp(flow);
 
-        if(UserStore.getInstance().getToken() == null){
+        if (peoplegatchiPrefs.getBoolean("firstRun", true)) {
+
+            SharedPreferences.Editor editor = peoplegatchiPrefs.edit();
+            editor.putBoolean("firstRun", false);
+            editor.apply();
 
             History newHistory = History.single(new CreateStage());
             flow.setHistory(newHistory, Flow.Direction.REPLACE);
