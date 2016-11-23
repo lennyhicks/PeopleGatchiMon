ab19e41a79f165d862b071d4bdec86b95d532e81
SheaMaynard <weirdalgurl@yahoo.com>
2016-11-22 21:29:10 -0500

    Death Functional

--- app/src/main/java/com/PeopleGatchi/Stages/DeathStage.java
+++ app/src/main/java/com/PeopleGatchi/Stages/DeathStage.java
@@ -4,18 +4,18 @@ import android.app.Application;
 
 import com.PeopleGatchi.PeopleGatchiApplication;
 import com.PeopleGatchi.R;
-import com.PeopleGatchi.Riggers.VerticalSlideRigger;
+import com.PeopleGatchi.Riggers.FadeRigger;
 
 /**
  * Created by eaglebrosi on 11/21/16.
  */
 
 public class DeathStage extends IndexedStage {
-    public final VerticalSlideRigger rigger;
+    public final FadeRigger rigger;
 
     public DeathStage(Application context){
         super(DeathStage.class.getName());
-        this.rigger = new VerticalSlideRigger(context);
+        this.rigger = new FadeRigger(context);
     }
 
     public DeathStage(){
