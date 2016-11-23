e7eb7d6aa0ab3d8ef8e6fbf91618201f3254991a
evbrosi <evbrosi@gmail.com>
2016-11-23 11:44:07 -0500

    Going through the Create and Death and Education view and committed out the code and cleaned it up.

--- app/src/main/java/com/PeopleGatchi/Views/DeathView.java
+++ app/src/main/java/com/PeopleGatchi/Views/DeathView.java
@@ -25,8 +25,6 @@ public class DeathView extends RelativeLayout {
     private Context context;
     private Flow flow;
 
-
-
     @Bind(R.id.rip)
     TextView restInPeace;
 
@@ -46,20 +44,16 @@ public class DeathView extends RelativeLayout {
         super.onFinishInflate();
         ButterKnife.bind(this);
 
+        // Get the name for the tombstone.
         flow = PeopleGatchiApplication.getMainFlow();
-
         condolences.setText(StatusControls.getName());
-
-//        condolences.setText("Poor sweet "+ StatusControls.getName()+ " has passed away.\n Let's hope they are gently resting \n waiting for the time when you will meet again.");
     }
 
     @OnClick(R.id.play_again)
     public void playAgain() {
+        // Restarts the game and puts you back to the create screen.
         StatusControls.resetGame();
         History newHistory = History.single(new CreateStage());
         flow.setHistory(newHistory, Flow.Direction.REPLACE);
-
-
     }
-
 }
--- app/src/main/java/com/PeopleGatchi/Views/EducationView.java
+++ app/src/main/java/com/PeopleGatchi/Views/EducationView.java
@@ -65,6 +65,7 @@ public class EducationView extends LinearLayout {
     }
 
     @OnClick(R.id.increase_science)
+    // Increases your Science score with a random number between 1 and 5.
     public void changeScience() {
         int increase = ((int) (Math.random() * 5) + 1);
         StatusControls.setScienceEd(increase);
@@ -72,6 +73,7 @@ public class EducationView extends LinearLayout {
     }
 
     @OnClick(R.id.increase_math)
+    // Increases your Math score with a random number between 1 and 5.
     public void changeMath() {
         int increase = ((int) (Math.random() * 5) + 1);
         StatusControls.setMathEd(increase);
@@ -79,28 +81,44 @@ public class EducationView extends LinearLayout {
     }
 
     @OnClick(increase_philo)
+    // Increases your Philosophy score with a random number between 0 and 6. It's Philosophy, sometimes you don't learn nothing.
     public void changePhilo() {
-        int increase = ((int) (Math.random() * 5) + 1);
+        int increase = ((int) (Math.random() * 6));
         StatusControls.setPhiloEd(increase);
         pokeStats();
     }
 
     @OnClick(R.id.home_button)
+    // On the FAB click it takes you back to the Home View.
     public void homeButton() {
         flow = PeopleGatchiApplication.getMainFlow();
         flow.goBack();
     }
 
     public void pokeStats() {
+        // This is the character sheet. It prints out the Philosophy, Math and Education scores.
         philoLevel.setText("Your Philosophy Score is: " + StatusControls.getPhiloEd());
         mathLevel.setText("Your Maths score is: " + StatusControls.getMathEd());
         scienceLevel.setText("Your Science score is: " + StatusControls.getScienceEd());
-        // passes time.
+        // This calls the method that passes the time.
         HomeView.workSchoolDay();
-        statusCheck();
+        checkDisable();
     }
 
-    public void statusCheck() {
+    public void checkDisable() {
+        // This looks to see if any of the statuses are in danger of being too low (so you don't work yourself to death).
+        // It toast a warning message and then calls the disable button.
+        if ((StatusControls.getHungerLevel() <= 5) || (StatusControls.getPooLevel() <= 5) || (StatusControls.getPeeLevel() <= 5)
+                || (StatusControls.getHygieneLevel() <= 5) || (StatusControls.getThirstLevel() <= 5)
+                || (StatusControls.getRestLevel() <= 5)) {
+            setWarningMessage();
+            Toast.makeText(context, warningMessage, Toast.LENGTH_SHORT).show();
+            disableButton();
+        }
+    }
+
+    public void setWarningMessage() {
+        // Sets the warning message for when any status are low.
         if (StatusControls.getHungerLevel() <= 5) {
             warningMessage = "Your baby is super hungry!\n Go fed them!";
         } else if (StatusControls.getPooLevel() <= 5) {
@@ -114,19 +132,10 @@ public class EducationView extends LinearLayout {
         } else if (StatusControls.getRestLevel() <= 5) {
             warningMessage = "Your baby needs a nap!\n Put them to bed!";
         }
-        checkDisable();
-    }
-
-    public void checkDisable() {
-        if ((StatusControls.getHungerLevel() <= 5) || (StatusControls.getPooLevel() <= 5) || (StatusControls.getPeeLevel() <= 5)
-                || (StatusControls.getHygieneLevel() <= 5) || (StatusControls.getThirstLevel() <= 5)
-                || (StatusControls.getRestLevel() <= 5)) {
-            Toast.makeText(context, warningMessage, Toast.LENGTH_SHORT).show();
-            disableButton();
-        }
     }
 
     public void disableButton() {
+        // This disables the buttons for 15 seconds (to prevent a user from killing themselves).
         handler = new Handler();
         handlerTask = new Runnable() {
             @Override
